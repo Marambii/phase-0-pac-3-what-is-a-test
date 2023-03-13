4f999dff5ed5a18dc54ee1e1a6c0cc137216920b
Baruch Marambi Mwenda <marambib2019@gmail.com>
2023-03-13 08:31:36 +0300

    Assignment done

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const height = 74;
 const message = `${name} is ${height} inches tall`;
 
 module.exports = {name, height, message};
+
+const message = `${name} is ${height} inches tall`;
+console.log(message);
+
